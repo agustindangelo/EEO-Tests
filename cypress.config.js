0641ca39ac8240189f3a8daf9f8be42f16f9fcb9
ENDAVA\JLostumbo <Julian.Lostumbo@endava.com>
2022-06-07 10:42:33 -0300

    more cypress configs

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        
+      })
     },
+    baseUrl: 'http://localhost:3000/',
+    watchForFileChanges: true,
+    defaultCommandTimeout: 4000,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: false
+
   },
 });
--- cypress/e2e/ex.spec.cy.js
+++ cypress/e2e/ex.spec.cy.js
@@ -2,4 +2,7 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('task', () => {
+    cy.visit('https://example.cypress.io')
+  })
 })
