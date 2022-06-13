e1c9a4b0de80bfe0eb64f9b35fc3b8f5d88c8444
agustindangelo <dangeloagustinariel@gmail.com>
2022-06-13 14:06:57 -0300

    added cypress-dotenv

--- cypress.config.js
+++ cypress.config.js
@@ -7,12 +7,12 @@ module.exports = defineConfig({
         
       })
     },
+    specPattern: 'cypress/fixtures/e2e/**/*.spec.js',
     baseUrl: 'http://localhost:3000/',
     watchForFileChanges: true,
     defaultCommandTimeout: 4000,
     viewportHeight: 1080,
     viewportWidth: 1920,
     video: false
-
   },
 });
