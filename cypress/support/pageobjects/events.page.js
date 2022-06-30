dd643ef0bab2484dc788423b83d95b51d2d810dc
ENDAVA\JLostumbo <Julian.Lostumbo@endava.com>
2022-06-30 12:01:23 -0300

    documentation added for the page objects

--- cypress/support/pageobjects/events.page.js
+++ cypress/support/pageobjects/events.page.js
@@ -1,68 +1,144 @@
 /// <reference types="Cypress"/>
 const Page = require('./page');
 
+/**
+* Methods, properties and selectors for the main page in the view of a comms user, where the draft and public events are listed
+*/
 class Events extends Page{
 
     url = 'events'
 
+    /**
+     * Gets the '+ Create New Event' button
+     */
     get createEventBtn(){
         return cy.get('main .maxContainer').eq(0).find('button')
     }
     
+    /**
+     * Gets the user button in the navbar
+     */
     get userBtn(){
         return cy.get('button.initials')
     }
 
+    /**
+     * Gets the user name displayed after clicking the user button in the navbar
+     */
     get userName(){
         return cy.get('.actions > span.actionsHeader')
     }
 
+    /**
+     * Gets the 'Log Out' displayed after clicking the user button in the navbar
+     */
     get logOutBtn(){
         return cy.contains('Log out')
     }
 
+    /**
+     * Receives as a parameter the name of an existing event in order to return the card of that selected event
+     * @param {string} name 
+     * @returns the DOM element of event's card
+     */
+     getEventByName = (name) => cy.contains(name).parent('div').parent('div')
+
+     /**
+      * Receives as a parameter the name of an existing event in order to return the state of that selected event
+      * @param {string} name 
+      * @returns the DOM element of the event's state
+      */
+     getEventStateByEventName(name){
+         return this.getEventByName(name).children('div').eq(3).children('div')
+     }
+ 
+     /**
+      * Receives as a parameter the name of an existing event in order to return the date of that selected event
+      * @param {string} name 
+      * @returns the DOM element of the event's date
+      */
+     getEventDateByEventName(name){
+         return this.getEventByName(name).children('div').eq(1).children('div')
+     }
+ 
+     /**
+      * Receives as a parameter the name of an existing event in order to return the number of attendees of that selected event
+      * @param {string} name 
+      * @returns the DOM element of the event's attendees
+      */
+     getEventAttendeesByEventName(name){
+         return this.getEventByName(name).children('div').eq(5).children('div')
+     }
+
+    /**
+     * Receives as a parameter the number of the event in order to get the card related to that event. DRAFT events are displayed first sorted by creating date
+     * @param {number} nth 
+     * @returns the DOM element of event's card
+     */
     getNthEventName = (nth) => cy.get('div .eventCardTitle').eq(nth-1)
-    
+
+    /**
+     * Receives as a parameter the number of the event in order to get the name of the selected event. DRAFT events are displayed first sorted by creating date
+     * @param {number} nth 
+     * @returns the DOM element of the event's name
+     */
     getNthEvent = (nth) => cy.get('div.maxContainer').eq(1).children('div').eq(nth-1)
 
+    /**
+     * Receives as a parameter the number of the event in order to get the state of the selected event. DRAFT events are displayed first sorted by creating date
+     * @param {number} nth 
+     * @returns the DOM element of the event's state
+     */
     getNthEventState(nth){
         return cy.get(`div.maxContainer .eventCardBadge`).eq(nth-1)
     }
 
+    /**
+     * Receives as a parameter the number of the date in order to get the state of the selected event. DRAFT events are displayed first sorted by creating date
+     * @param {number} nth 
+     * @returns the DOM element of the event's date
+     */
     getNthEventDate(nth){
         return cy.get(`.eventCardDate > span`).eq((nth))
     }
 
-    getEventByName = (name) => cy.contains(name).parent('div').parent('div')
-
-    getEventStateByEventName(name){
-        return this.getEventByName(name).children('div').eq(3).children('div')
-    }
-
-    getEventDateByEventName(name){
-        return this.getEventByName(name).children('div').eq(1).children('div')
-    }
-
-    getEventAttendeesByEventName(name){
-        return this.getEventByName(name).children('div').eq(5).children('div')
-    }
-
+    /**
+     * Receives as a parameter the number of the date in order to get the option button of the selected event. DRAFT events are displayed first sorted by creating date
+     * @param {number} nth 
+     * @returns the DOM element of the event's options
+     */
     getNthEventOptions(nth){
-        return cy.get(`div.maxContainer .sc-jIkXHa.RvbSq:nth-child(${nth}) svg`).last()
+        return cy.get(`div.maxContainer .sc-jIkXHa.RvbSq:nth-child(${nth}) svg`).last() //the selector could be better
     }
 
+    /**
+     * Receives as a parameter the number of the date in order to get the title of the selected event. DRAFT events are displayed first sorted by creating date
+     * @param {number} nth 
+     * @returns the DOM element of the event's title
+     */
     getTitleOfNthEvent(nth) {
         return this.getNthEvent(nth).find('.eventCardTitle')
     }
 
+    /**
+     * It searches for the 'Export attendee list' when the event options are expanded 
+     * @returns the DOM element the export option
+     */
     get exportEventOption(){
         return cy.contains('Export attendee list')
     }
 
+    /**
+     * It searches for the 'Delete' when the event options are expanded 
+     * @returns the DOM element the delete option
+     */
     get deleteEventOption(){
         return cy.contains('Delete')
     }
 
+    /**
+     * The prompt and its fields and elements referred to the deletion of a chosen event
+     */
     deletePrompt = {
         message: () => cy.contains('Delete this event?'),
         label: () => cy.contains('This action cannot be restored'),
@@ -70,39 +146,70 @@ class Events extends Page{
         noOption: () => cy.contains('No, cancel'),
     }
 
+    /**
+     *Opens a sub page of the page
+     */
     navigate(){
         super.navigate(this.url)
     }
 
+    /**
+     * It clicks the '+ Create New Event' button
+     */
     createNewEvent(){
         this.createEventBtn.click()
     }
 
+    /**
+     * It clicks the user button
+     */
     clickUserOption(){
         this.userBtn.click()
     }
 
+    /**
+     * It clicks the 'Log Out' button
+     */
     clickLogOut(){
         this.logOutBtn.click()
     }
 
+    /**
+     * It opens the user options and then the 'Log Out' is clicked in order to logout an user
+     */
     logOut(){
         this.clickUserOption()
         this.clickLogOut()
     }
     
+    /**
+     * It gets the nth event and the its opened in order to explore its details
+     * @param {number} event number of the event. DRAFT events are displayed first sorted by creating date
+     */
     openEventDetails(event){
         this.getNthEvent(event).click()
     }
 
+    /**
+     * It gets the nth event and the its options are opened
+     * @param {number} event number of the event. DRAFT events are displayed first sorted by creating date
+     */
     openEventOptions(event){
         this.getNthEventOptions(event).click()
     }
 
+    /**
+     * It clicks the export option in order to export the attendee list of a chosen event
+     * @param {number} event number of the event. 
+     */
     exportAtendeeList(){
         this.exportEventOption.click()
     }
 
+    /**
+     * It clicks the delete option in order to delete a DRAFT event
+     * @param {number} event number of the event. 
+     */
     clickDeleteEventOption(event){
         this.openEventOptions(event)
         this.deleteEventOption.click()
--- cypress/support/util.js
+++ cypress/support/util.js
@@ -2,7 +2,10 @@
 // import { faker } from '@faker-js/faker'
 // export const getRandomEmail = () => faker.internet.email()
 
-// Generates a random "Gmail"
+/**
+ * It generates a random valid email that follows the 'xxxx@gmail.com' format
+ * @returns {string} The generated email
+ */
 export const getRandomEmail = () => {
   let chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
   var string = '';
@@ -12,19 +15,35 @@ export const getRandomEmail = () => {
   return string + '@gmail.com'
 }
 
+/**
+ * It generates an integer number between 'min' and 'max'. This function is mainly used to generate a random day or month for a date. 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns The generated integer in string format
+ */
 export const getInteger = (min, max) => {
     let value = Math.floor(Math.random() * (max - min + 1) ) + min;
     if ((value + '').length == 1) { return '0' + value }
     return value + ''
   }
 
+/**
+ * It takes an hour in 24H format and turns it to an 12H format
+ * @param {number} hour 
+ * @returns 
+ */
 export const hourToAmPm = (hour) => {
     if (hour == 0 ) { hour = 12 }
     if (hour > 12) { hour = hour - 12 }
     return hour
   }
 
+/**
+ * It generates a random invalid email.
+ * @returns {string} The generated email wiht an invalid format
+ */
   export const invalidEmail = () => {
+    //more invalid emails can be added to the array
     let emails = [' ', 'asdsa', '1234', '@.com', 'asdsa.com', 'asdsa@sadsad', 'sadsad@asdsad.', '.com', 'asas@', '@sadsa', '@sadsa.']
     let randomPos = Math.floor(Math.random() * (emails.length-1) ) + 1
     let email = emails[randomPos]
